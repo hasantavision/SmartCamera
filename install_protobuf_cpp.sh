22bf8d6f21506facbe2d2c812ff1e8d7153ab762
hasantavision <81632134+hasantavision@users.noreply.github.com>
2022-09-26 14:56:21 +0700

    Add files via upload
    protobuf cpp implementation for jetson

---
+++ install_protobuf_cpp.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+version=3.19.4
+
+folder=${HOME}/src
+mkdir -p $folder
+
+echo "** Install requirements"
+sudo apt-get install -y autoconf libtool
+
+echo "** Download protobuf-${version} sources"
+pushd $folder
+if [ ! -f protobuf-python-${version}.zip ]; then
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v${version}/protobuf-python-${version}.zip
+fi
+if [ ! -f protoc-${version}-linux-aarch_64.zip ]; then
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v${version}/protoc-${version}-linux-aarch_64.zip
+fi
+
+echo "** Install protoc"
+unzip protobuf-python-${version}.zip
+unzip protoc-${version}-linux-aarch_64.zip -d protoc-${version}
+sudo cp protoc-${version}/bin/protoc /usr/local/bin/protoc
+
+echo "** Build and install protobuf-${version} libraries"
+cd protobuf-${version}/
+./autogen.sh
+./configure --prefix=/usr/local
+make -j$(nproc)
+#make check  ### Disabled because "protobuf-test" would fail (JetPack-4.6)!
+sudo make install
+sudo ldconfig
+
+echo "** Update python3 protobuf module"
+# remove previous installation of python3 protobuf module
+sudo pip3 uninstall -y protobuf
+cd python/
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+python3 setup.py build --cpp_implementation
+python3 setup.py test --cpp_implementation
+python3 setup.py bdist_wheel --cpp_implementation
+cp dist/*.whl /opt
+sudo pip3 install dist/*.whl
+# sudo python3 setup.py install --cpp_implementation
+
+popd
+
+echo "** Build protobuf-${version} successfully"
